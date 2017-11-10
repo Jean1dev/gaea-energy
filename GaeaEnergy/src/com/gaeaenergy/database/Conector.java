1ef873a4c234f498a0e43828e7330022cf17554f
Jean <>
2017-11-10 20:15:17 -0200

    Criado o banco e o pacote connector

---
+++ GaeaEnergy/src/com/gaeaenergy/database/Conector.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gaeaenergy.database;
+
+
+import com.gaeaenergy.stream.WriteToFile;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author comp8
+ */
+public class Conector {
+    
+    private static final String driver_db = "org.hsqldb.jdbcDriver";
+    private static final String url = "jdbc:hsqldb:hsql://localhost/gaeadb";
+    private static final String db_user = "sa";
+    private static final String db_password = "";
+    
+    public static Connection/**/ getConnection() throws ClassNotFoundException{
+        Connection connection = null;
+
+            Class.forName(driver_db);
+        try {
+            connection = DriverManager.getConnection(url, db_user, db_password);
+        } catch (SQLException ex) {
+            WriteToFile log = new WriteToFile();
+            log.jogaPilhaNoArquivo(ex, true);
+        }
+        
+        
+        return connection;
+    }
+}
