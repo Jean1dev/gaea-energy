5470a22c2b7c3f98c5223590e623794d5f865191
Jean <jeanlucafp@gmail.com>
2019-04-04 12:48:31 -0300

    servlet com java (-__-)
    so pra loco aprender isso na 8 fase da faculdade

---
+++ WebRestaurante/src/java/servelet/TestServlet.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servelet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author jeanfernandes
+ */
+public class TestServlet extends HttpServlet{
+    
+}
